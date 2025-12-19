ea477a4f6670bc3d9eaa1245b2f8836b204827bc
bala-amypo <bala@amypo.in>
2025-12-19 08:58:11 +0000

    User commit

--- src/main/java/com/example/demo/exception/SupplierNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class SupplierNotFoundException extends RuntimeException {
-    public SupplierNotFoundException(String message) {
-        super(message);
-    }
-}
