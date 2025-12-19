0dd8a8829ad838073a0ca13566320874ce421fd6
bala-amypo <bala@amypo.in>
2025-12-19 08:43:11 +0000

    User commit

--- src/main/java/com/example/demo/service/SupplierProfileService.java
+++ src/main/java/com/example/demo/service/SupplierProfileService.java
@@ -1,9 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.SupplierProfile;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface SupplierProfileService {
+
     SupplierProfile createSupplier(SupplierProfile supplier);
-    SupplierProfile getSupplierById(Long id) throws SupplierNotFoundException;
-    SupplierProfile getBySupplierCode(String supplierCode);
+
+    SupplierProfile getSupplierById(Long id);
+
+    Optional<SupplierProfile> getSupplierBySupplierCode(String supplierCode);
+
     List<SupplierProfile> getAllSuppliers();
+
     SupplierProfile updateSupplierStatus(Long id, boolean active);
 }
