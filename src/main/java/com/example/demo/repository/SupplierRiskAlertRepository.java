6cb231c0ac696096fc11c1ff10e8ef1376a9f9ba
bala-amypo <bala@amypo.in>
2025-12-19 08:31:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/SupplierRiskAlertRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.SupplierRiskAlert;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SupplierRiskAlertRepository
+        extends JpaRepository<SupplierRiskAlert, Long> {
+
+    List<SupplierRiskAlert> findBySupplierId(Long supplierId);
+}
