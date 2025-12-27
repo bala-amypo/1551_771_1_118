2e13f1ff119ab91cacbc6f38d718c9e9baef3ff8
bala-amypo <bala@amypo.in>
2025-12-27 05:46:22 +0000

    User commit

--- src/main/java/com/example/demo/service/DelayScoreService.java
+++ src/main/java/com/example/demo/service/DelayScoreService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface DelayScoreService {
 
-    DelayScoreRecord calculateScore(Long poId);
+    DelayScoreRecord calculateDelayScore(Long poId);
 
     List<DelayScoreRecord> getAllScores();
 }
