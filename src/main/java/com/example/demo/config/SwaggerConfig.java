e3a261711aa71b9018fb541ff4a47a8634abe79e
bala-amypo <bala@amypo.in>
2025-12-28 13:26:39 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import.java.utilList;
+import.java.util.List;
 import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class SwaggerConfig {
